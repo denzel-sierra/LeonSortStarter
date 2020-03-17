c3863276cb96d63a9fb18541e4d4043ba4846faf
Denzel Sierra <denzel.sierra@ujcv.edu.hn>
2020-03-17 17:35:42 -0600

    Hice el arreglo aún más tonto y comenté el ShuffleSort porque no funciona

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -7,7 +7,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int[] elementos = {2797, 971, 3436, 6250, 221, 6893, 3373, 7343, 205, 2986, 9122, 8594, 469, 8753, 3387, 8446, 2027, 4581, 6235, 7186, 5124, 7052, 3318, 3290, 6975, 935, 8926, 1027, 1342, 901, 89, 4829, 5073, 2380, 9259, 3441, 4607, 1815, 8428, 2650, 1855, 7826, 154, 6125, 9963, 7308, 7531, 9307, 81, 6919};
+        int[] elementos = {27972797, 9712797, 34362797, 62502797, 2212797,
+                68932797, 33732797, 73432797, 2052797, 29862797, 91222797,
+                85942797, 4692797, 87532797, 33872797, 84462797, 20272797,
+                45812797, 62352797, 71862797, 51242797, 70522797, 33182797,
+                32902797, 69752797, 9352797, 89262797, 10272797, 13422797,
+                9012797, 892797, 48292797, 50732797, 23802797, 92592797,
+                34412797, 46072797, 18152797, 84282797, 26502797, 18552797,
+                78262797, 1542797, 61252797, 99632797, 73082797, 75312797,
+                93072797, 812797, 69192797};
         ShuffleSort shuffle_s = new ShuffleSort();
         BubleSort bubble_s = new BubleSort();
         MergeSort merge_s = new MergeSort();
@@ -16,7 +24,7 @@ public class Main {
 
         System.out.println(Arrays.toString(elementos));
 
-        System.out.println("ShuffleSort tarda "+ shuffle_s.sort(elementos)+ " milisegundos");
+        //System.out.println("ShuffleSort tarda "+ shuffle_s.sort(elementos)+ " milisegundos");
         System.out.println("BubbleSort tarda "+ bubble_s.sort(elementos)+ " milisegundos");
         System.out.println("MergeSort tarda "+ merge_s.sort(elementos)+ " milisegundos");
         System.out.println("HeapSort tarda "+ heap_s.sort(elementos)+ " milisegundos");
