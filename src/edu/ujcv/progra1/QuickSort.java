ebcb45e1d215e24c0784f41616a9cc2ddc4e533a
Denzel Sierra <denzel.sierra@ujcv.edu.hn>
2020-03-17 16:50:39 -0600

    Algoritmo del QuickSort completo

--- src/edu/ujcv/progra1/QuickSort.java
+++ src/edu/ujcv/progra1/QuickSort.java
@@ -4,13 +4,40 @@ public class QuickSort implements SortTester{
     @Override
     public long sort(int[] array) {
         long start = System.currentTimeMillis();
-        quicksort(array);
+        quicksort(array, 0, array.length-1);
 
         long end = System.currentTimeMillis();
 
         return end - start;
     }
 
-    private void quicksort(int[] array) {
+    int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1);
+        for (int j=low; j<high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+
+    void quicksort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+            quicksort(arr, low, pi-1);
+            quicksort(arr, pi+1, high);
+        }
     }
 }
