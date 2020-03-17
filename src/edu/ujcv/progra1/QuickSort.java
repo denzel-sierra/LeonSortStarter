1efeebb0c9367228733353d0960313e2d83fd037
Denzel Sierra <denzel.sierra@ujcv.edu.hn>
2020-03-17 16:38:36 -0600

    Cree el método QuickSort e implementé el Sort Tester

--- src/edu/ujcv/progra1/QuickSort.java
+++ src/edu/ujcv/progra1/QuickSort.java
@@ -1,4 +1,16 @@
 package edu.ujcv.progra1;
 
-public class QuickSort {
+public class QuickSort implements SortTester{
+    @Override
+    public long sort(int[] array) {
+        long start = System.currentTimeMillis();
+        quicksort(array);
+
+        long end = System.currentTimeMillis();
+
+        return end - start;
+    }
+
+    private void quicksort(int[] array) {
+    }
 }
